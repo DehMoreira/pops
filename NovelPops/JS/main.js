99f5675cd316434a73aa1bc1ae42f119148adb62
DehMoreira <d-moreira@hotmail.com>
2021-10-08 20:04:45 -0300

    Alterando de repositorio, front novo

---
+++ NovelPops/JS/main.js
@@ -0,0 +1,174 @@
+var novelas = [{tit: "da cor do pecado", img: "https://s2.glbimg.com/Ks1Irvid6I_BtaEfUGIc2qK88HY=/362x536/https://s2.glbimg.com/wnVQPAEuAmapGSTp5Udyhq1wWn0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2021/G/P/aFb7JiT0upDq8ymoC4qA/2021-da-cor-pecado-poster-1-.jpg",count:0, pont: 10},
+                {tit: "estrela guia", img: "https://s2.glbimg.com/XbaE7qM2feCYAxo4V3suOLLBtxg=/362x536/https://s2.glbimg.com/Wx1Hv9fmgGpFe-Pl7auL7T-taaY=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/6/i/M6ZigNSrW8JQbk5q87OQ/2020-937-estrela-guia-poster.jpg",count:0, pont: 10},
+                {tit: "boogie oogie", img: "https://dg31sz3gwrwan.cloudfront.net/poster/302017/1282384-4-optimized.jpg", count:0, pont: 10},
+                {tit: "avenida brasil", img: "https://artworks.thetvdb.com/banners/posters/5c45f8216454c_t.jpg",count:0, pont: 10},
+                {tit: "bela a feia", img: "https://artworks.thetvdb.com/banners/posters/5bf112c44a122_t.jpg", count:0, pont: 10},
+                {tit: "paraiso", img: "https://artworks.thetvdb.com/banners/series/320259/posters/5f539ab9eebe8_t.jpg", count:0, pont: 10},
+                {tit: "por amor", img: "https://artworks.thetvdb.com/banners/posters/5d548471949bf_t.jpg", count:0, pont: 10},
+                {tit: "o outro lado do paraiso", img: "https://artworks.thetvdb.com/banners/posters/5b8c20326295a_t.jpg", count:0, pont: 10},
+                {tit: "pantanal", img: "https://artworks.thetvdb.com/banners/posters/319508-3_t.jpg", count:0, pont: 10},
+                {tit: "passione", img: "https://artworks.thetvdb.com/banners/posters/325693-2_t.jpg", count:0, pont: 10},
+                {tit: "a gata comeu", img: "https://artworks.thetvdb.com/banners/posters/292185-1_t.jpg", count:0, pont: 10},
+                {tit: "cheias de charme", img: "https://artworks.thetvdb.com/banners/posters/302903-2_t.jpg", count:0, pont: 10},
+                {tit: "bom sucesso", img: "https://artworks.thetvdb.com/banners/posters/5d5484d0d68b2_t.jpg", count:0, pont: 10},
+                {tit: "sinha moca", img: "https://artworks.thetvdb.com/banners/posters/5b1fc5ce9c2c0_t.jpg", count:0, pont: 10},
+                {tit: "verao 90", img: "https://artworks.thetvdb.com/banners/posters/5c4b6cc9d5dcf_t.jpg", count:0, pont: 10},
+                {tit: "porto dos milagres", img: "https://artworks.thetvdb.com/banners/v4/series/292458/posters/604b82f5d4c28_t.jpg",count:0, pont: 10},
+                {tit: "o profeta", img: "https://artworks.thetvdb.com/banners/posters/330173-1_t.jpg", count:0, pont: 10},
+                {tit: "era uma vez", img: "https://artworks.thetvdb.com/banners/series/330538/posters/6026a9228557e_t.jpg", count:0, pont: 10},
+                {tit: "floribella", img: "https://artworks.thetvdb.com/banners/posters/314538-1_t.jpg", count:0, pont: 10},
+                {tit: "duas caras", img: "https://artworks.thetvdb.com/banners/series/292270/posters/5fd0a02fa21b3_t.jpg", count:0, pont: 10},
+                {tit: "a escrava isaura", img: "https://artworks.thetvdb.com/banners/posters/5b9dced25a245_t.jpg", count:0, pont: 10},
+                {tit: "cama de gato", img: "https://artworks.thetvdb.com/banners/posters/330174-1_t.jpg", count:0, pont: 10},
+                {tit: "cabocla", img: "https://artworks.thetvdb.com/banners/v4/series/329879/posters/60e5a3cd194ee_t.jpg", count:0, pont: 10},
+                {tit: "roque santeiro", img: "https://artworks.thetvdb.com/banners/series/289188/posters/62049675_t.jpg", count:0, pont: 10},
+                {tit: "pe na jaca", img: "https://artworks.thetvdb.com/banners/posters/329510-1_t.jpg",count:0, pont: 10},
+                {tit: "pequena travessa", img: "https://artworks.thetvdb.com/banners/posters/323379-5_t.jpg", count:0, pont: 10},
+                {tit: "o beijo do vampiro", img: "https://artworks.thetvdb.com/banners/posters/329553-2_t.jpg", count:0, pont: 10},
+                {tit: "alto astral", img: "https://artworks.thetvdb.com/banners/posters/301888-2_t.jpg", count:0, pont: 10},
+                {tit: "irmaos coragem", img: "https://artworks.thetvdb.com/banners/posters/292184-1_t.jpg", count:0, pont: 10},
+                {tit: "o setimo guardiao", img: "https://artworks.thetvdb.com/banners/posters/5bec2ad1b0a53_t.jpg", count:0, pont: 10},
+                {tit: "verdades secretas", img: "https://artworks.thetvdb.com/banners/posters/297463-6_t.jpg", count:0, pont: 10},
+                {tit: "o rei do gado", img: "https://artworks.thetvdb.com/banners/posters/290257-3_t.jpg", count:0, pont: 10},
+                {tit: "a favorita", img: "https://artworks.thetvdb.com/banners/series/306101/posters/5ec97f778dcd1_t.jpg", count:0, pont: 10},
+                {tit: "caminhos do coracao", img: "https://artworks.thetvdb.com/banners/posters/309565-8_t.jpg", count:0, pont: 10},
+                {tit: "chocolate com pimenta", img: "https://artworks.thetvdb.com/banners/posters/313809-2_t.jpg", count:0, pont: 10},
+                {tit: "morde e assopra", img: "https://s2.glbimg.com/8oMAFNjGfgvJfz7Vlpm-mZY4w6w=/362x536/https://s2.glbimg.com/H0xO4y0GGGawCgr6A8keQodxS7g=/s3.glbimg.com/v1/AUTH_180b9dd048d9434295d27c4b6dadc248/media_kit/0c/fc/e84ef623ac03f7a9f0c965f79afe.jpg", count:0, pont: 10},
+                {tit: "esmeralda", img: "https://artworks.thetvdb.com/banners/series/325823/posters/5f08ee79abe25_t.jpg", count:0, pont: 10},
+                {tit: "orfaos da terra", img: "https://artworks.thetvdb.com/banners/posters/5ca9306969130_t.jpg", count:0, pont: 10},
+                {tit: "negocio da china", img: "https://artworks.thetvdb.com/banners/posters/330588-1_t.jpg", count:0, pont: 10},
+                {tit: "o cravo e a rosa", img: "https://artworks.thetvdb.com/banners/posters/5cd049621aafc_t.jpg", count:0, pont: 10},
+                {tit: "a padroeira", img: "https://artworks.thetvdb.com/banners/posters/327403-3_t.jpg", count:0, pont: 10},
+                {tit: "cumplices de um resgate", img: "https://artworks.thetvdb.com/banners/posters/301115-1_t.jpg", count:0, pont: 10},
+                {tit: "eterna magia", img: "https://artworks.thetvdb.com/banners/posters/331375-5_t.jpg", count:0, pont: 10},
+                {tit: "kubanacan", img: "https://artworks.thetvdb.com/banners/v4/series/329250/posters/607c8b778aa2d_t.jpg", count:0, pont: 10},
+                {tit: "uga uga", img: "https://artworks.thetvdb.com/banners/posters/321382-3_t.jpg", count:0, pont: 10},
+                {tit: "marisol", img: "https://artworks.thetvdb.com/banners/posters/325819-3_t.jpg", count:0, pont: 10},
+                {tit: "eta mundo bom", img: "https://artworks.thetvdb.com/banners/posters/304525-1_t.jpg", count:0, pont: 10},
+                {tit: "as filhas da mae", img: "https://artworks.thetvdb.com/banners/posters/5ca9306969130_t.jpg", count:0, pont: 10},
+                {tit: "a viagem", img: "https://artworks.thetvdb.com/banners/posters/289316-3_t.jpg", count:0, pont: 10},
+                {tit: "o clone", img: "https://artworks.thetvdb.com/banners/posters/281962-5_t.jpg", count:0, pont: 10},
+                {tit: "prova de amor", img: "https://artworks.thetvdb.com/banners/posters/328277-3_t.jpg", count:0, pont: 10},
+                {tit: "a regra do jogo", img: "https://artworks.thetvdb.com/banners/posters/299277-3_t.jpg", count:0, pont: 10},
+                {tit: "um anjo caiu do ceu", img: "https://artworks.thetvdb.com/banners/posters/330892-6_t.jpg", count:0, pont: 10},
+                {tit: "o tempo nao para", img: "https://artworks.thetvdb.com/banners/posters/5b1089b8db50f_t.jpg", count:0, pont: 10},
+                {tit: "a vida da gente", img: "https://artworks.thetvdb.com/banners/v4/series/298775/posters/611dcd3105515_t.jpg", count:0, pont: 10},
+                {tit: "carrossel", img: "https://artworks.thetvdb.com/banners/posters/298505-1_t.jpg", count:0, pont: 10},
+                {tit: "alma gemea", img: "https://artworks.thetvdb.com/banners/posters/329922-1_t.jpg", count:0, pont: 10},
+                {tit: "senhora do destino", img: "https://artworks.thetvdb.com/banners/posters/311065-2_t.jpg", count:0, pont: 10},
+                {tit: "sete pecados", img: "https://artworks.thetvdb.com/banners/posters/311232-2_t.jpg",count:0, pont: 10},
+                {tit: "coracao de estudante", img: "https://artworks.thetvdb.com/banners/posters/330533-1_t.jpg", count:0, pont: 10},
+                {tit: "deus salve o rei", img: "https://artworks.thetvdb.com/banners/posters/336427-18_t.jpg", count:0, pont: 10},
+                {tit: "saramandaia", img: "https://artworks.thetvdb.com/banners/posters/271028-1_t.jpg", count:0, pont: 10},
+                {tit: "mulheres de areia", img: "https://artworks.thetvdb.com/banners/v4/series/281319/posters/60624b2447a05_t.jpg", count:0, pont: 10},
+                {tit: "explode coracao", img: "https://artworks.thetvdb.com/banners/posters/5b43634d827bf_t.jpg", count:0, pont: 10}]
+var pontos = 0;
+
+window.onload = () => {
+
+    /* ações para os botões */
+    const play_btn = document.getElementById('play-btn');
+    const play_btn_icon = document.getElementById('play-icon');
+    const prev_btn = document.getElementById('prev-btn');
+    const next_btn = document.getElementById('next-btn');
+
+/* ações para o player */
+const audio_player = document.getElementById('music-player');
+
+
+play_btn.addEventListener('click', TogglePlaySong);
+next_btn.addEventListener('click', () => ChangeSong());
+prev_btn.addEventListener('click', () => ChangeSong(false));
+
+InitPlayer();
+
+function InitPlayer() {
+    var titulo = document.getElementById("song-title").firstChild.nodeValue;
+    UpdatePlayer(titulo);
+}
+
+/* ação para atualizar o player */
+function UpdatePlayer(i) {
+    var aux = ("musicas/msc") + (i) + (".mp3");
+    audio_player.src = aux;
+}
+
+function TogglePlaySong() {
+    if (audio_player.paused) {
+        audio_player.play();
+        play_btn_icon.classList.remove('fa-play');
+        play_btn_icon.classList.add('fa-pause');
+    } else {
+        audio_player.pause();
+        play_btn_icon.classList.add('fa-play');
+        play_btn_icon.classList.remove('fa-pause');
+
+    }
+}
+
+}
+
+
+function Verifica(palpite){
+
+    var titulo = document.getElementById("song-title").firstChild.nodeValue;
+    var imagem = document.getElementById("song-image");
+
+    indice = titulo - 1;
+
+    if(novelas[indice].tit == palpite.value){
+        var aux = ("url(") + (novelas[indice].img) + (")");
+        imagem.style.backgroundImage = aux;
+        imagem.style.backgroundSize = "cover"; 
+        
+        //imagem.style.backgroundRepeat = "no-repeat";
+        //imagem.setAttribute('src', novelas[indice].img); //troca imagem
+        //remov.parentNode.removeChild(remov); //remove itens
+        pontos = novelas[indice].pont + pontos;
+        //pontuacao.innerHTML= pontos;
+        return false;
+    }
+
+    else{
+        
+        if(novelas[indice].count < 10) {
+            novelas[indice].pont = novelas[indice].pont - 1;
+        }
+        novelas[indice].count+=1;
+        imagem.style.backgroundImage= "url('imagens/x.jpg";
+        return false;
+    }
+}
+
+/*
+function Verifica(indice,titulo,palpite,cor,remov,pontuacao)
+{   
+    
+    if(novelas[indice].tit == palpite.value){
+        var aux = ("url(") + (novelas[indice].img) + (")");
+        cor.style.backgroundImage = aux;
+        cor.style.backgroundSize = "300px 380px"; 
+        cor.style.backgroundRepeat = "no-repeat";
+        //imagem.setAttribute('src', novelas[indice].img);
+        remov.parentNode.removeChild(remov);
+        pontos = novelas[indice].pont + pontos;
+        pontuacao.innerHTML= pontos;
+        return false;
+    }
+
+    else{
+
+        if(novelas[indice].count < 10) {
+            novelas[indice].pont = novelas[indice].pont - 1;
+        }
+
+        //Atualiza o Card
+        novelas[indice].count+=1;
+        cor.style.color = "white";
+        cor.style.background = "red";
+        titulo.innerHTML= novelas[indice].count;
+
+        return false;
+    }
+*/
+
